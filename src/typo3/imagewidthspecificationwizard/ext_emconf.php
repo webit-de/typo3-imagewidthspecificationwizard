a18a7766692040f014b901cb829b3a895367f958
Dan Untenzu <untenzu@webit.de>
2013-04-04 10:55:10 +0200

    [TASK] Erhöhe Version auf 0.2.2

--- src/typo3/imagewidthspecificationwizard/ext_emconf.php
+++ src/typo3/imagewidthspecificationwizard/ext_emconf.php
@@ -1,22 +1,22 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "imagewidthspecificationwizard".
-#
-# Auto generated 18-08-2011 09:47
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "imagewidthspecificationwizard".
+ *
+ * Auto generated 04-04-2013 10:23
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Wizard to specify the field imagewidth in contentelements',
 	'description' => 'This backendextension attaches a selectfield to contentelements like textpic or images to provide a set of default values for the width of an image (eg.: fullsize, half of the content, small teaser etc.).',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '0.2.1',
-	'dependencies' => '',
+	'version' => '0.2.2',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -42,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"502c";s:49:"class.tx_imagewidthspecificationwizard_wizard.php";s:4:"5aad";s:12:"ext_icon.gif";s:4:"ee32";s:17:"ext_localconf.php";s:4:"1e5e";s:14:"ext_tables.php";s:4:"a9f9";s:13:"locallang.xml";s:4:"06be";s:16:"pageTSconfig.txt";s:4:"f1bb";s:14:"doc/manual.pdf";s:4:"dea0";s:14:"doc/manual.sxw";s:4:"2836";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"502c";s:49:"class.tx_imagewidthspecificationwizard_wizard.php";s:4:"5aad";s:12:"ext_icon.gif";s:4:"ee32";s:17:"ext_localconf.php";s:4:"1e5e";s:14:"ext_tables.php";s:4:"a9f9";s:13:"locallang.xml";s:4:"06be";s:16:"pageTSconfig.txt";s:4:"f1bb";s:14:"doc/manual.pdf";s:4:"9eab";s:14:"doc/manual.sxw";s:4:"648f";}',
+	'suggests' => array(
+	),
 );
 
 ?>
