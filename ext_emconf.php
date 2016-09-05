cdf614fb5366554c766c0d2f444d4308d9185f41
Dan Untenzu <untenzu@webit.de>
2016-09-05 17:26:17 +0200

    [TASK] Set compatible TYPO3 versions
    Extension now compatible with TYPO3 CMS 6 and 7.

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-7.0.99',
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.6.99',
 			'cms' => '',
 		),
 		'conflicts' => array(
