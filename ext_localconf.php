a97c1a317d915b31d1a7699cb2102d4531959ff1
Lidia Demin <demin@webit.de>
2018-09-12 10:41:59 +0200

    [FEATURE] Move page TScofnig to configuration folder
    Refs #49403

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:imagewidthspecificationwizard/pageTSconfig.txt">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:imagewidthspecificationwizard/Configuration/TSconfig/page-root.tsconfig">');
